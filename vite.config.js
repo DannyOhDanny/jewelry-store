784d47abdf95a6ea1ab992baec9a2bd098428ff3
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-03-04 14:58:28 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/jewelry-store',
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
